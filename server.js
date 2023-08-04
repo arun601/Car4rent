5542ebd65cd0191f1c2ad032418e40ab1efaccfc
Arun Yadav <78892809+arun601@users.noreply.github.com>
2023-08-03 23:16:38 -0700

    Update server

--- server.js
+++ server.js
@@ -11,18 +11,12 @@ app.use('/api/bookings/' , require('./routes/bookingsRoute'))
 
 const path = require('path')
 
-if(process.env.NODE_ENV==='production')
-{
+app.use(express.static(path.join(__dirname, "./client/build")));
 
-    app.use('/' , express.static('client/build'))
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
 
-    app.get('*' , (req , res)=>{
-
-          res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
-
-    })
-
-}
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
