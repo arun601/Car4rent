75634ebd303d4c5ccc5f546361d2c5dce5344052
Arun Yadav <78892809+arun601@users.noreply.github.com>
2023-08-03 23:35:37 -0700

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 function connectDB(){
 
-    mongoose.connect('mongodb+srv://arun1:p2XXG9ZnkJDxJgR4@cluster1.xpaeqvs.mongodb.net/carrent' , {useUnifiedTopology: true , useNewUrlParser: true})
+    mongoose.connect('mongodb+srv://arun:iKVtNjitR7Ob65v0@cluster1.xpaeqvs.mongodb.net/carrent' , {useUnifiedTopology: true , useNewUrlParser: true})
 
     const connection = mongoose.connection
 
